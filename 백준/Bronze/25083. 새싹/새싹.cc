edefc444b24c45a2386018f15026d890fd33a3aa
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2024-04-07 21:14:52 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+#include "iostream"
+#include "ctime"
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/\n"
+            "      |\n"
+            "      |" << endl;
+    return 0;
+}
