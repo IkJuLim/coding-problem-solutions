48f160b8af4163eff0e50c8a27dbce00ed1b10a0
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2024-12-29 23:26:29 +0900

    Time: 1 ms, Memory: 48 MB - LeetHub

--- 10076-382-547-number-of-provinces/10076-382-547-number-of-provinces.java
+++ 10076-382-547-number-of-provinces/10076-382-547-number-of-provinces.java
@@ -1,23 +1,21 @@
 class Solution {
-  List<Integer> nodes = new LinkedList<>();
+  boolean[] visited;
   public int findCircleNum(int[][] isConnected) {
+    visited = new boolean[isConnected.length];
     int ret = 0;
     for(int i = 0; i < isConnected.length; i++){
-      nodes.add(i);
-    }
-    while (!nodes.isEmpty()){
-      if(dfs(nodes.get(0), isConnected)){
+      if(dfs(i, isConnected)){
         ret++;
       }
     }
     return ret;
   }
 
-  public boolean dfs(Integer target, int[][] isConnected){
-    if(!nodes.contains(target)){
+  public boolean dfs(int target, int[][] isConnected){
+    if(visited[target]){
       return false;
     }
-    nodes.remove(target);
+    visited[target] = true;
     for(int i = 0; i < isConnected.length; i++){
       if(target != i && isConnected[target][i] == 1){
         dfs(i, isConnected);
