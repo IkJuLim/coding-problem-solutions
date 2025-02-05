6c0e1c60734f574987c6e484d26bcf15aab521be
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2025-02-05 16:21:07 +0900

    Time: 1 ms (8.73%), Space: 40.2 MB (87.26%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,40 +1,24 @@
 class Solution {
-  class Pair {
-    int a;
-    int b;
-
-    public Pair(int a, int b) {
-      this.a = a;
-      this.b = b;
-    }
-  }
-
+  int[][] dp;
+  
   public int uniquePaths(int m, int n) {
-    Queue<Pair> q = new LinkedList<>();
-    int[][] cnt = new int[m + 1][n + 1];
-    boolean[][] visited = new boolean[m + 1][n + 1];
-    int x = m - 1, y = n - 1;
-    cnt[x][y] = 1;
-    q.add(new Pair(x, y));
-
-    while (!q.isEmpty()) {
-      Pair poll = q.poll();
-      x = poll.a;
-      y = poll.b;
-      if (visited[x][y]) {
-        continue;
-      }
-      visited[x][y] = true;
-      if(x - 1 >= 0) {
-        q.add(new Pair(x - 1, y));
-      }
-      if(y - 1 >= 0)
-        q.add(new Pair(x, y-1));
-
-      cnt[x][y] = cnt[x][y] + cnt[x + 1][y] + cnt[x][y + 1];
-
+    dp = new int[m][n];
+    for (int i = 0; i < m; i++) {
+      dp[i][0] = 1;
     }
-
-    return cnt[0][0];
+    for (int i = 0; i < n; i++) {
+      dp[0][i] = 1;
+    }
+    
+    for (int i = 2; i < m + n - 1; i++) {
+      for (int j = 1; j < i; j++) {
+        if (i - j >= m || j >= n) {
+          continue;
+        }
+        dp[i - j][j] = dp[i - j - 1][j] + dp[i - j][j - 1];
+      }
+    }
+    
+    return dp[m - 1][n - 1];
   }
 }
