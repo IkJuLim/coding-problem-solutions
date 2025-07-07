1659a58b6b71839fd134ab594fb398a167b159b9
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2025-07-07 18:46:24 +0900

    Time: 0 ms (100%), Space: 44.1 MB (98.81%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.java
+++ 0427-construct-quad-tree/0427-construct-quad-tree.java
@@ -5,10 +5,14 @@ class Solution {
     }
 
     public Node construct(int[][] grid, int topLeft_x, int topLeft_y, int bottomRight_x, int bottomRight_y) {
-        if (bottomRight_x - topLeft_x == 1) {
-            return new Node(grid[topLeft_x][topLeft_y] == 1, true);
-        }
+
         boolean isLeaf = true;
+        Node node = new Node(grid[topLeft_x][topLeft_y] == 1, isLeaf);
+
+        if (bottomRight_x - topLeft_x == 1) {
+            return node;
+        }
+
         for (int i = topLeft_x; i < bottomRight_x; i++) {
             for (int j = topLeft_y; j < bottomRight_y; j++) {
                 if (grid[topLeft_x][topLeft_y] != grid[i][j]) {
@@ -17,33 +21,28 @@ class Solution {
             }
         }
 
-        Node node;
-
         if (!isLeaf) {
-            node = new Node(grid[topLeft_x][topLeft_y] == 1, isLeaf,
-                    construct(grid,
+            node.isLeaf = isLeaf;
+            node.topLeft = construct(grid,
                             topLeft_x,
                             topLeft_y,
                             (topLeft_x + bottomRight_x) / 2,
-                            (topLeft_y + bottomRight_y) / 2),
-                    construct(grid,
+                            (topLeft_y + bottomRight_y) / 2);
+            node.topRight = construct(grid,
                             topLeft_x,
                             (topLeft_y + bottomRight_y) / 2,
                             (topLeft_x + bottomRight_x) / 2,
-                            bottomRight_y),
-                    construct(grid,
+                            bottomRight_y);
+            node.bottomLeft = construct(grid,
                             (topLeft_x + bottomRight_x) / 2,
                             topLeft_y,
                             bottomRight_x,
-                            (topLeft_y + bottomRight_y) / 2),
-                    construct(grid,
+                            (topLeft_y + bottomRight_y) / 2);
+            node.bottomRight = construct(grid,
                             (topLeft_x + bottomRight_x) / 2,
                             (topLeft_y + bottomRight_y) / 2,
                             bottomRight_x,
-                            bottomRight_y)
-            );
-        } else {
-            node = new Node(grid[topLeft_x][topLeft_y] == 1, isLeaf);
+                            bottomRight_y);
         }
 
         return node;
