a4870eb5138f2426203a209a02a2182639d9ee16
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2025-09-20 21:38:44 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ LeetCode/Medium/0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -1,19 +1,6 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        int ret = 0;
-        int cnt = 0;
-        for(int i = 0; i < s.size(); i++){
-            if(cnt >= 0 && s[i] == '('){
-                cnt++;
-            }
-            else if(cnt > 0 && s[i] == ')'){
-                cnt--;
-            }
-            else if(cnt == 0 && s[i] == ')'){
-                ret++;
-            }
-        }
-        return cnt + ret;
+        
     }
 };
