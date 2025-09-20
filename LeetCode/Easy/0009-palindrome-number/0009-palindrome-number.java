5fb6cf572c9f6ef2a3cd32c38aff8771979254cf
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2025-09-20 20:42:13 +0900

    Time: 7 ms (12.87%), Space: 44 MB (60.74%) - LeetHub

---
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,9 @@
+class Solution {
+  public boolean isPalindrome(int x) {
+    if (x < 0) {
+      return false;
+    }
+    StringBuilder sb = new StringBuilder(Integer.toString(x));
+    return sb.toString().equals(sb.reverse().toString());
+  }
+}
