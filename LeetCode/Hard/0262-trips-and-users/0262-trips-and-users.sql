0321c47a40555a0d0d4d2051847e3eb2ea62d31d
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2025-10-13 15:29:31 +0900

    Time: 516 ms (68.31%), Space: 0B (100%) - LeetHub

--- LeetCode/Hard/0262-trips-and-users/0262-trips-and-users.sql
+++ LeetCode/Hard/0262-trips-and-users/0262-trips-and-users.sql
@@ -1,28 +1,10 @@
--- # Write your MySQL query statement below
--- SELECT T.request_at AS 'DAY', 
---     ROUND(COUNT(IF(T.status != 'completed', TRUE, NULL))/COUNT(*), 2) AS 'Cancellation Rate'
--- FROM Trips AS T
---     LEFT JOIN Users AS UC ON UC.users_id = T.client_id
---     LEFT JOIN Users AS UD ON UD.users_id = T.driver_id
--- WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
---     AND UC.banned = 'No'
---     AND UD.banned = 'No'
--- GROUP BY T.request_at;
-
-
-
-WITH cte_all AS(
-    SELECT request_at
-        , SUM(CASE WHEN status <> 'completed' THEN 1 ELSE 0 END) as cancel
-        , COUNT(*) as total 
-    FROM Trips AS T
-        LEFT JOIN Users AS UC ON UC.users_id = T.client_id
-        LEFT JOIN Users AS UD ON UD.users_id = T.driver_id
-    WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-        AND UC.banned = 'No'
-        AND UD.banned = 'No'
-    GROUP BY request_at
-)
-SELECT request_at as Day
-    , ROUND(cancel/total, 2) as "Cancellation Rate"
-FROM cte_all
+# Write your MySQL query statement below
+SELECT T.request_at AS 'DAY', 
+    ROUND(COUNT(IF(T.status != 'completed', TRUE, NULL))/COUNT(*), 2) AS 'Cancellation Rate'
+FROM Trips AS T
+    LEFT JOIN Users AS UC ON UC.users_id = T.client_id
+    LEFT JOIN Users AS UD ON UD.users_id = T.driver_id
+WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+    AND UC.banned = 'No'
+    AND UD.banned = 'No'
+GROUP BY T.request_at;
