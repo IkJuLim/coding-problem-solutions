2c335a44df3bfc59cf8cde3e86a38f3fb89e6d7c
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2025-10-13 15:31:26 +0900

    Time: 466 ms (96.43%), Space: 0B (100%) - LeetHub

--- LeetCode/Hard/0262-trips-and-users/0262-trips-and-users.sql
+++ LeetCode/Hard/0262-trips-and-users/0262-trips-and-users.sql
@@ -1,10 +1,41 @@
-# Write your MySQL query statement below
-SELECT T.request_at AS 'DAY', 
-    ROUND(COUNT(IF(T.status != 'completed', TRUE, NULL))/COUNT(*), 2) AS 'Cancellation Rate'
+-- # Write your MySQL query statement below
+-- SELECT T.request_at AS 'DAY', 
+--     ROUND(COUNT(IF(T.status != 'completed', TRUE, NULL))/COUNT(*), 2) AS 'Cancellation Rate'
+-- FROM Trips AS T
+--     LEFT JOIN Users AS UC ON UC.users_id = T.client_id
+--     LEFT JOIN Users AS UD ON UD.users_id = T.driver_id
+-- WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+--     AND UC.banned = 'No'
+--     AND UD.banned = 'No'
+-- GROUP BY T.request_at;
+
+
+SELECT T.request_at AS Day
+    , ROUND(COUNT(IF(T.status != 'completed', TRUE, NULL))/COUNT(*), 2) AS "Cancellation Rate"
 FROM Trips AS T
-    LEFT JOIN Users AS UC ON UC.users_id = T.client_id
-    LEFT JOIN Users AS UD ON UD.users_id = T.driver_id
+    LEFT JOIN Users AS UC ON T.client_id = UC.users_id
+    LEFT JOIN Users AS UD ON T.driver_id = UD.users_id
 WHERE T.request_at BETWEEN '2013-10-01' AND '2013-10-03'
-    AND UC.banned = 'No'
-    AND UD.banned = 'No'
-GROUP BY T.request_at;
+    AND UC.banned = "NO"
+    AND UD.banned = "NO"
+GROUP BY T.request_at
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
