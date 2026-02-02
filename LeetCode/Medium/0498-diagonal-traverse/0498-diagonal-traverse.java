6f3b6087b8eb8a40dc235b88ad693ca56df313ed
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-02-02 13:04:40 +0900

    Time: 5 ms (32.29%), Space: 48.2 MB (42.49%) - LeetHub

--- LeetCode/Medium/0498-diagonal-traverse/0498-diagonal-traverse.java
+++ LeetCode/Medium/0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -5,13 +5,13 @@ class Solution {
         boolean isYIncreaseDir = true;
 
         for (int i = 0; i < mat.length + mat[0].length; i++) {
-            int x = isYIncreaseDir ? i : 0;
-            int y = !isYIncreaseDir ? i : 0;
+            int x = isYIncreaseDir ? Math.min(i, mat.length - 1) : i - Math.min(i, mat[0].length - 1);
+            int y = !isYIncreaseDir ? Math.min(i, mat[0].length - 1) : i - Math.min(i, mat.length - 1);
             while ((isYIncreaseDir ? x : y) >= 0) {
                 if ((x >= mat.length || y >= mat[0].length)) {
                     x = isYIncreaseDir ? x-1 : x+1;
                     y = isYIncreaseDir ? y+1 : y-1;
-                    continue;
+                    break;
                 }
                 ret[len] = mat[x][y];
 
