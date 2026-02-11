fe3778af4a3276ea53d4cba787de5787cf5b7bd0
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-02-11 16:41:34 +0900

    Time: 8 ms (9.32%), Space: 47.2 MB (42.08%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,23 +1,14 @@
-import java.util.Stack;
-
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> s = new Stack<>();
-        s.add(asteroids[0]);
-        for (int i = 1; i < asteroids.length; i++) {
+        for (int i = 0; i < asteroids.length; i++) {
             if (s.size() > 0 && asteroids[i] < 0 && s.peek() > 0) {
-                if (asteroids[i] * -1 == s.peek()) {
-                    s.pop();
-                    continue;
-                } else if (asteroids[i] * -1 > s.peek()) {
+                if (asteroids[i] * -1 == s.peek()) s.pop();
+                else if (asteroids[i] * -1 > s.peek()) {
                     s.pop();
                     i--;
-                    continue;
-                } else {
-                    continue;
                 }
-            }
-            s.add(asteroids[i]);
+            } else s.add(asteroids[i]);
         }
         return s.stream().mapToInt(i -> i).toArray();
     }
