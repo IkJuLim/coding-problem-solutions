d36072ffce3a654c86478f53c63cf0771f8d8e99
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-02-24 22:22:48 +0900

    Time: 8 ms (5.54%), Space: 45 MB (7.52%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -1,23 +1,27 @@
 class Solution {
-    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    HashMap<Integer, List<List<Integer>>> map = new HashMap<>();
 
     public List<List<Integer>> verticalTraversal(TreeNode root) {
-        int currC = 0;
         dfs(root, 0, 0);
 
         List<List<Integer>> ret = new LinkedList<>();
 
         List<Integer> keys = map.keySet().stream().sorted().toList();
         for (int key : keys) {
-            ret.add(map.get(key).stream().sorted().toList());
+            ret.add(map.get(key).stream()
+                    .flatMap(innerList -> innerList.stream().sorted())
+                    .collect(Collectors.toList()));
         }
 
         return ret;
     }
 
     private void dfs(TreeNode node, int row, int col) {
-        List<Integer> valList = map.getOrDefault(col, new LinkedList<>());
-        valList.add(node.val);
+        List<List<Integer>> valList = map.getOrDefault(col, new LinkedList<>());
+        while (valList.size() - 1 < row) {
+            valList.add(new LinkedList<>());
+        }
+        valList.get(row).add(node.val);
         map.put(col, valList);
         if (node.left != null) {
             dfs(node.left, row+1, col-1);
