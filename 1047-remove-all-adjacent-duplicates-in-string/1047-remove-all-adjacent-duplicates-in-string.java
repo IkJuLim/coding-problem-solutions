35b2e93c225142b9a8e89a19903feba3a855e4ab
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-02-25 21:33:47 +0900

    Time: 15 ms (70.43%), Space: 46.3 MB (96.7%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (sb.length() != 0 && sb.charAt(sb.length() - 1) == s.charAt(i)) {
+                sb.delete(sb.length() - 1, sb.length());
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
