b782991e7c180494b636a52f2e92ec4921cc53a8
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-03-19 18:02:15 +0900

    [Gold IV] Title: N-Queen, Time: 5636 ms, Memory: 14680 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int ret;
+    private static int[] queens;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ret = 0;
+        queens = new int[n];
+        for (int i = 0; i < n; i++) {
+            queens[i] = -1;
+        }
+        recall(n, 0);
+        System.out.println(ret);
+
+    }
+
+    private static void recall(int n, int row) {
+        if (row >= n) {
+            ret++;
+        }
+        for (int col = 0; col < n; col++) {
+            if (isPosable(row, col)) {
+                queens[row] = col;
+                recall(n, row + 1);
+            }
+        }
+    }
+
+    private static boolean isPosable(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (i == row ||
+                    queens[i] == col ||
+                    i + queens[i] == row + col ||
+                    i - queens[i] == row - col)
+                return false;
+        }
+        return true;
+    }
+}
