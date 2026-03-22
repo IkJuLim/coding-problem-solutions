2e6e213e278d48476760db7a7cda209fb3b31e9d
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-03-22 13:00:00 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Integer.valueOf(st.nextToken()), b = Integer.valueOf(st.nextToken()), c = Integer.valueOf(st.nextToken());
+        System.out.println(pow(a, b, c));
+    }
+
+    public static long pow(long a, long b, long c) {
+        if(b == 1) {
+            return a % c;
+        }
+        long temp = pow(a, b / 2, c);
+
+        if(b % 2 == 1) {
+            return (temp * temp % c) * a % c;
+        }
+        return temp * temp % c;
+
+    }
+}
