a8a7133411997cb91c6aaf70ba1ba060da786e5a
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-03-28 21:22:37 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0018-4sum/0018-4sum.java
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -1,17 +1,28 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
-        Set<List<Integer>> ret = new HashSet<>();
+        List<List<Integer>> ret = new ArrayList<>();
         for (int idx1 = 0; idx1 < nums.length - 3; idx1++) {
+            if (idx1 != 0 && nums[idx1] == nums[idx1 - 1]){
+                continue;
+            }
             for (int idx2 = idx1 + 1; idx2 < nums.length - 2; idx2++) {
+                if (idx2 != idx1 + 1 && nums[idx2] == nums[idx2 - 1]){
+                    continue;
+                }
                 int start = idx2 + 1, end = nums.length - 1;
                 long tar = (long) target - nums[idx1] - nums[idx2];
-                System.out.println("tar = " + tar);
                 while (start < end) {
                     if (tar == nums[start] + nums[end]) {
                         ret.add(List.of(nums[idx1], nums[idx2], nums[start], nums[end]));
                         start++;
                         end--;
+                        while (start < end && nums[start] == nums[start - 1]) {
+                            start++;
+                        }
+                        while (start < end && nums[end] == nums[end + 1]) {
+                            end--;
+                        }
                     } else if (tar > nums[start] + nums[end]) {
                         start++;
                     } else if (tar < nums[start] + nums[end]) {
@@ -20,6 +31,6 @@ class Solution {
                 }
             }
         }
-        return ret.stream().toList();
+        return ret;
     }
 }
