b6c723da0667c2f0ed1aacd40942eb1d4f32e8bb
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-03-28 21:18:35 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0018-4sum/0018-4sum.java
+++ LeetCode/Medium/0018-4sum/0018-4sum.java
@@ -3,9 +3,7 @@ class Solution {
         Arrays.sort(nums);
         Set<List<Integer>> ret = new HashSet<>();
         for (int idx1 = 0; idx1 < nums.length - 3; idx1++) {
-            if (idx1 != 0 && nums[idx1] == nums[idx1 - 1]) continue;
             for (int idx2 = idx1 + 1; idx2 < nums.length - 2; idx2++) {
-                if (idx2 != idx1 + 1 && nums[idx2] == nums[idx2 - 1]) continue;
                 int start = idx2 + 1, end = nums.length - 1;
                 long tar = (long) target - nums[idx1] - nums[idx2];
                 System.out.println("tar = " + tar);
