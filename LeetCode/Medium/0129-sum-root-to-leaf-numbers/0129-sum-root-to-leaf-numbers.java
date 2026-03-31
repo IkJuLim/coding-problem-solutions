3fcb143e83fb778a164943f3c852e7d56ed9a03d
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-03-31 17:55:17 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
+++ LeetCode/Medium/0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -1,26 +1,23 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
+    int ret;
     public int sumNumbers(TreeNode root) {
-        int ret = 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            TreeNode node = q.poll();
-            if (node.left != null) {
-                node.left.val = node.val * 10 + node.left.val;
-                q.add(node.left);
-            }
-            if (node.right != null) {
-                node.right.val = node.val * 10 + node.right.val;
-                q.add(node.right);
-            }
-            if(node.right == null && node.left == null) {
-                ret += node.val;
-            }
+        ret = 0;
+        calc(root, 0);
+        return ret;
+    }
+
+    private void calc(TreeNode currNode, int currSum) {
+        if (currNode == null) {
+            return;
         }
 
-        return ret;
+        currSum = currSum * 10 + currNode.val;
+        if (currNode.left == null && currNode.right == null) {
+            ret += currSum;
+            return;
+        }
+
+        calc(currNode.left, currSum);
+        calc(currNode.right, currSum);
     }
 }
