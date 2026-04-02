4e9bc93232123873eda6ed6f6f8de552afb6343a
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-04-02 20:44:22 +0900

    Time: 18 ms (24.85%), Space: 58.4 MB (65.06%) - LeetHub

---
+++ LeetCode/Medium/0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -0,0 +1,25 @@
+class Solution {
+    List<TreeNode> ret;
+    Map<String, Integer> map;
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        ret = new ArrayList<>();
+        map = new HashMap();
+
+        dfs(root);
+        
+        return ret;
+    }
+
+    private String dfs(TreeNode root) {
+        if (root == null) {
+            return "";
+        }
+        String pathStr = root.val + "(" + dfs(root.left) + ")" + "(" + dfs(root.right) + ")";
+        map.put(pathStr, map.getOrDefault(pathStr, 0) + 1);
+        if (map.get(pathStr) == 2) {
+            ret.add(root);
+        }
+        return pathStr;
+    }
+}
