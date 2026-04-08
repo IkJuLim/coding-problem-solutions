f7ad49a674d939bafaf377f86dcaea6cd9db1dde
Lim Ik Ju <79306035+IkJuLim@users.noreply.github.com>
2026-04-08 17:40:21 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Medium/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
+++ LeetCode/Medium/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
@@ -1,24 +1,22 @@
 class Solution {
     List<String> ret;
-    Map<String, List<int[]>> map;
+    Map<String, List<Integer>> map;
 
     public List<String> alertNames(String[] keyName, String[] keyTime) {
         ret = new ArrayList<>();
         map = new HashMap<>();
 
         for (int i = 0; i < keyName.length; i++) {
-            int[] time = Arrays.stream(keyTime[i].split(":"))
+            int[] parseTime = Arrays.stream(keyTime[i].split(":"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            List<int[]> list = map.getOrDefault(keyName[i], new ArrayList<>());
-            list.add(time);
+            List<Integer> list = map.getOrDefault(keyName[i], new ArrayList<>());
+            list.add(parseTime[0] * 60 + parseTime[1]);
             map.put(keyName[i], list);
         }
 
         for (String name : map.keySet()) {
-            List<int[]> timeList = map.get(name);
-            timeList.sort((a, b) -> (a[0] * 60 + a[1]) - (b[0] * 60 + b[1]));
-
+            List<Integer> timeList = map.get(name);
             find(name, timeList);
         }
 
@@ -26,11 +24,10 @@ class Solution {
         return ret;
     }
 
-    private void find(String name, List<int[]> timeList) {
+    private void find(String name, List<Integer> timeList) {
+        Collections.sort(timeList);
         for (int i = 0; i < timeList.size() - 2; i++) {
-            int[] time1 = timeList.get(i);
-            int[] time2 = timeList.get(i + 2);
-            if ((time2[0] * 60 + time2[1]) - (time1[0] * 60 + time1[1]) <= 60) {
+            if (timeList.get(i + 2) - timeList.get(i) <= 60) {
                 ret.add(name);
                 return;
             }
